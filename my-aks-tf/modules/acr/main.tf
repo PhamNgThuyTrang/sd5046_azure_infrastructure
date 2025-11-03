38326c20db308b05d4391fa0857c0cd722e096bc
trangpham <pntt220200@gmail.com>
2025-11-04 01:57:39 +0700

    fix: edit acr tf file

--- my-aks-tf/modules/acr/main.tf
+++ my-aks-tf/modules/acr/main.tf
@@ -7,15 +7,9 @@ terraform {
   }
 }
 resource "azurerm_container_registry" "acr" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.region
-  sku                 = "Standard"
-}
-
-resource "azurerm_role_assignment" "k8s" {
-  principal_id                     = var.principal_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
+    name                = var.name
+    resource_group_name = var.resource_group_name
+    location            = var.region
+    sku                 = "Premium"
+    admin_enabled       = true
 }
