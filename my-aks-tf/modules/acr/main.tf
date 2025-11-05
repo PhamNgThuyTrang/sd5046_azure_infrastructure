c113c86fe0f197db63c24f2c0d0b24fdc644c8fe
trangpham <pntt220200@gmail.com>
2025-11-05 15:35:14 +0700

    [SD5046] fix: attaching a Container Registry to a Kubernetes Cluster

--- my-aks-tf/modules/acr/main.tf
+++ my-aks-tf/modules/acr/main.tf
@@ -13,3 +13,10 @@ resource "azurerm_container_registry" "acr" {
     sku                 = "Premium"
     admin_enabled       = true
 }
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id                     = var.principal_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
